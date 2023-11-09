6936fb3280dd1855ce3066f0adf0bf382aeccecc
Galondor <johnyjoejoe55@gmail.com>
2023-11-08 19:52:12 -0500

    Init

--- src/App.js
+++ src/App.js
@@ -1,24 +1,39 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState, useEffect } from 'react';
+
 
 function App() {
+  const [remainingTime, setRemainingTime] = useState({});
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      const now = new Date();
+      const targetDate = new Date('December 17, 2023 13:00:00 EST');
+      const timeDiff = targetDate - now;
+
+      if (timeDiff <= 0) {
+        clearInterval(intervalId);
+        setRemainingTime({ days: 0, hours: 0, minutes: 0, seconds: 0 });
+      } else {
+        const days = Math.floor(timeDiff / (1000 * 60 * 60 * 24));
+        const hours = Math.floor((timeDiff / (1000 * 60 * 60)) % 24);
+        const minutes = Math.floor((timeDiff / 1000 / 60) % 60);
+        const seconds = Math.floor((timeDiff / 1000) % 60);
+
+        setRemainingTime({ days, hours, minutes, seconds });
+      }
+    }, 1000);
+
+    return () => clearInterval(intervalId);
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <body>
+      <div className="App">
+        <h1 className="Countdown">{remainingTime.days} : {remainingTime.hours} : {remainingTime.minutes} : {remainingTime.seconds}</h1>
+        <p className="Countdown_text">To Release</p>
+      </div>
+    </body>
   );
 }
 
