2978e1adf10bfaf5073ec2f212a378918b18b3ef
Galondor <johnyjoejoe55@gmail.com>
2023-11-15 17:58:30 -0500

    Code Box

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import { Analytics } from '@vercel/analytics/react';
 
 function App() {
   const [remainingTime, setRemainingTime] = useState({});
+  const [code, setCode] = useState('');
 
   useEffect(() => {
     const intervalId = setInterval(() => {
@@ -28,11 +29,43 @@ function App() {
     return () => clearInterval(intervalId);
   }, []);
 
+  const handleCodeChange = (event) => {
+    setCode(event.target.value);
+  };
+  fetch('/Astral Odyssey/AstralOdyssey.exe')
+    .then(response => {
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
+      }
+      const contentType = 'application/octet-stream';
+      return response.blob();
+    })
+    .then(blob => {
+      // ...
+    })
+    .catch(error => {
+      console.error('There was a problem with the fetch operation:', error);
+    });
+
   return (
     <body>
       <div className="App">
         <h1 className="Countdown">{remainingTime.days} : {remainingTime.hours} : {remainingTime.minutes} : {remainingTime.seconds}</h1>
         <p className="Countdown_text">To Release</p>
+        <input className="Password" type="text" value={code} onChange={handleCodeChange} onKeyDown={(event) => {
+          if (event.key === 'Enter') {
+            switch (code) {
+              case 'AstralOdysseyPreAlphaV0.3.7.0release':
+                alert('Correct!');
+                setCode('');
+                break;
+              default:
+                alert('Incorrect!');
+                setCode('');
+                break;
+            }
+          }
+        }} />
       </div>
       <Analytics />
     </body>
