b5060270de74a2b95756f18a5f8aca5c49fe7f76
Galondor <johnyjoejoe55@gmail.com>
2023-11-16 15:17:34 -0500

    nvm

--- client/src/App.js
+++ client/src/App.js
@@ -17,7 +17,7 @@ function App() {
       responseType: "blob"
     }).then((res)=> {
       console.log(res);
-      FileDownload(res.data,"AstralOdyssey.zip")
+      FileDownload(res.data,"AstralOdyssey.exe")
     })
   }
 
