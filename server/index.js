35cf69b26431579dbfda849f39d90c9171578e56
Galondor <johnyjoejoe55@gmail.com>
2023-11-16 12:08:29 -0500

    Node

--- server/index.js
+++ server/index.js
@@ -2,10 +2,22 @@ const express = require('express');
 const cors = require('cors');
 
 const app = express();
+const PORT = 4000;
+
 app.use(cors());
 
+app.listen(PORT, () => {
+    console.log(`API listening on PORT ${PORT} `)
+  })
+
 app.get("/", (req, res) => {
+    res.send('API Running')
     res.download("AstralOdyssey.exe");
 });
 
-app.listen(4000);
+app.get('/about', (req, res) => {
+    res.send('This is my about route..... ')
+  })
+
+// Export the Express API
+module.exports = app;
