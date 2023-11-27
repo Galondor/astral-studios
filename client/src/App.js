4e4bb93051ce4ce9aece0f2728d93e3580d42ecf
Galondor <johnyjoejoe55@gmail.com>
2023-11-27 16:35:18 -0500

    Upd Again

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,17 @@
-import './App.css';
-import { useState, useEffect } from 'react';
-import { Analytics } from '@vercel/analytics/react';
-
+import "./App.css";
+import { useState, useEffect } from "react";
+import { Analytics } from "@vercel/analytics/react";
 
 function App() {
   const [remainingTime, setRemainingTime] = useState({});
   const [remainingTime2, setRemainingTime2] = useState({});
-  const [code, setCode] = useState('');
+  const [code, setCode] = useState("");
 
   useEffect(() => {
     const intervalId = setInterval(() => {
       const now = new Date();
-      const targetDate = new Date('December 17, 2023 13:00:00 EST');
-      const targetDate2 = new Date('December 4, 2023 19:00:00 EST');
+      const targetDate = new Date("December 17, 2023 13:00:00 EST");
+      const targetDate2 = new Date("December 4, 2023 19:00:00 EST");
       const timeDiff = targetDate - now;
       const timeDiff2 = targetDate2 - now;
 
@@ -44,39 +43,53 @@ function App() {
     return () => clearInterval(intervalId);
   }, []);
 
-
   const handleCodeChange = (event) => {
     setCode(event.target.value);
   };
 
   return (
     <body>
-      <div className="App">
-        <h1 className="Countdown">{remainingTime.days} : {remainingTime.hours} : {remainingTime.minutes} : {remainingTime.seconds}</h1>
-        <p className="Countdown_text">To Release</p>
-        <h1 className="Countdown">{remainingTime2.days} : {remainingTime2.hours} : {remainingTime2.minutes} : {remainingTime2.seconds}</h1>
-        <p className="Countdown_text">To Closed Beta</p>
-        <input
-          className="Password"
-          type="text"
-          value={code}
-          onChange={handleCodeChange}
-          onKeyDown={(event) => {
-            if (event.key === 'Enter') {
-              const accessCode = process.env.REACT_APP_ACCESS_KEY;
-              if (code === accessCode) {
-                alert('Correct!');
-                window.open(`${process.env.REACT_APP_GAME_LINK}`, '_blank');
-                setCode('');
-                return;
-              } else {
-                alert('Incorrect!');
-                setCode('');
-                return;
+      <div className='App'>
+        <div className='Container'>
+          <div className='Release'>
+            <h1 className='Countdown'>
+              {remainingTime.days} : {remainingTime.hours} :{" "}
+              {remainingTime.minutes} : {remainingTime.seconds}
+            </h1>
+            <p className='Countdown_text'>Release:</p>
+          </div>
+          <div className='Beta'>
+            <h1 className='Countdown'>
+              {remainingTime2.days} : {remainingTime2.hours} :{" "}
+              {remainingTime2.minutes} : {remainingTime2.seconds}
+            </h1>
+            <p className='Countdown_text'>Closed Beta:</p>
+          </div>
+        </div>
+        <div className='Codes'>
+          <h1 className='code_Label'>Got Codes?</h1>
+          <input
+            className='Password'
+            type='text'
+            value={code}
+            onChange={handleCodeChange}
+            onKeyDown={(event) => {
+              if (event.key === "Enter") {
+                const accessCode = process.env.REACT_APP_ACCESS_KEY;
+                if (code === accessCode) {
+                  alert("Correct!");
+                  window.open(`${process.env.REACT_APP_GAME_LINK}`, "_blank");
+                  setCode("");
+                  return;
+                } else {
+                  alert("Incorrect!");
+                  setCode("");
+                  return;
+                }
               }
-            }
-          }}
-        />
+            }}
+          />
+        </div>
       </div>
       <Analytics />
     </body>
